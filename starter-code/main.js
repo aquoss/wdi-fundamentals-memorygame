c3dd3e9f1f806637b0015e854b046715d73dcb5c
Amber <amberquoss@gmail.com>
2016-11-26 22:12:28 -0800

    created card flip logic and game reset

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,24 +1,47 @@
-console.log("JS file is connected to HTML! Woo!")
-
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
-/*if (cardOne === cardTwo) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var board = document.getElementById('game-board');
 
 var createCards = function() {
-	var board = document.getElementById('game-board');
-	for (var i=0; i<4; i++) {
-		var card = document.createElement('div');
-		card.className = 'card';
-		board.appendChild(card);
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+		board.appendChild(cardElement);
 	}
-};
+}
+
+var isTwoCards = function() {
+	if ((this.getAttribute('data-card')) === "queen") {
+		this.innerHTML = '<img src="queen.png" alt="Queen"/>';
+	} else {
+		this.innerHTML = '<img src="king.png" alt="King"/>';
+	}
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (cardsInPlay.length === 2) {
+		setTimeout(function() {
+			isMatch(cardsInPlay);
+		}, 500);
+	}
+}
+
+var isMatch = function() {
+	if (cardsInPlay[0]===cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+	cardsInPlay = [];
+}
 
 createCards();
+
+var reset = function() {
+	var resetCard = document.getElementsByClassName('card');
+	resetCard.innerHTML = "";
+};
+
+var button = document.getElementById('btn');
+button.addEventListener('click', reset);
 
